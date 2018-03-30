971b8f2070abd69e4f62a77c0adbc92cb0ba6c56
djulzz <julien.esposito@gmail.com>
2018-03-30 13:23:59 -0700

    Modified your SQL Script so it works
    Modified your SQL Script so it works

--- car_database.sql
+++ car_database.sql
@@ -15,13 +15,15 @@ create table CarInfo
 	mpg int(3)
 );
 
-insert into CarInfo values('Audi', 'R8 Coupe', 176050.00, 22);
-insert into CarInfo values('Lotus', 'Evora 400 Coupe', 103035.99, 24);
-insert into CarInfo values('Ferrari', 'F12 Berlinetta', 323,745, 13);
-insert into CarInfo values('Alfa Romeo', 'Stelvio SUV', 49000.00, 28);
+ALTER TABLE CarInfo AUTO_INCREMENT = 1;
+
+insert into CarInfo (car_make, car_model, price, mpg ) values('Audi', 'R8 Coupe', 176050.00, 22);
+insert into CarInfo (car_make, car_model, price, mpg ) values('Lotus', 'Evora 400 Coupe', 103035.99, 24);
+insert into CarInfo (car_make, car_model, price, mpg )  values('Ferrari', 'F12 Berlinetta', 323745.00, 13);
+insert into CarInfo (car_make, car_model, price, mpg )  values('Alfa Romeo', 'Stelvio SUV', 49000.00, 28);
 
 #this is how we inspect the structure of our table
 #describe CarInfo;
-ALTER TABLE Car AUTO_INCREMENT = 1;
+
 #check content
 #select * from CarInfo where mpg >= 15;
